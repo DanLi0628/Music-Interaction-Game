eb59304b037344ec76d4105b773eb6266e472814
Dan Li <dl4224@nyu.edu>
2020-04-30 12:29:20 +0800

    add effect

--- sketch.js
+++ sketch.js
@@ -1,24 +1,29 @@
 let wxp;
 let backgroundImage;
 let note;
+noteW = 40;
+noteH = 50;
 
-function preload(){
+function preload() {
     wxp = loadImage("background/五线谱.jpeg");
     backgroundImage = loadImage('background/1.png');
-    noteImage = loadImage('background/note.png');
+    note1 = loadImage('background/note.png');
+    note2 = loadImage('background/note2.png');
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     note = new Note();
+    
 }
 
 function draw() {
     background(255);
-    image(backgroundImage,0,0,windowWidth,windowHeight);
-    image(wxp,0,430,windowWidth,50);
+    image(backgroundImage, 0, 0, windowWidth, windowHeight);
+    image(wxp, 0, 430, windowWidth, 50);
     note.move();
     note.show();
+    note.change();
 }
 
 class Note {
@@ -31,6 +36,14 @@ class Note {
         this.y = this.y;
     }
     show() {
-        image(noteImage,this.x,this.y,40,50);
+        image(note1, this.x, this.y, 40, 50);
     }
+    change() {
+        if (this.x>160 && this.x <200){
+        image(note2, this.x, this.y, noteW, noteH);
+        noteW += 2;
+        noteH += 2.5;
+    }
+
+}
 }
