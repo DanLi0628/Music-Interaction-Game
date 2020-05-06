438c06e5e6da1226ddf6169f6de424a517dfc372
Dan Li <dl4224@nyu.edu>
2020-05-07 01:47:37 +0800

    more notes

--- sketch.js
+++ sketch.js
@@ -3,47 +3,46 @@ let backgroundImage;
 let note;
 noteW = 40;
 noteH = 50;
+let x1 = 1500;
+let y1 = 430;
+let x2 = 1700;
+let y2 = 430;
+
 
 function preload() {
     wxp = loadImage("background/五线谱.jpeg");
     backgroundImage = loadImage('background/1.png');
-    note1 = loadImage('background/note.png');
-    note2 = loadImage('background/note2.png');
+    note0 = loadImage('background/note0.png');
+    note1 = loadImage('background/note1.png');
+    noteFire = loadImage('background/note2.png');
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    note = new Note();
-    
 }
-
 function draw() {
-    background(255);
     image(backgroundImage, 0, 0, windowWidth, windowHeight);
     image(wxp, 0, 430, windowWidth, 50);
-    note.move();
-    note.show();
-    note.change();
-}
-
-class Note {
-    constructor(x, y) {
-        this.x = windowWidth;
-        this.y = 430;
-    }
-    move() {
-        this.x = this.x - 5;
-        this.y = this.y;
-    }
-    show() {
-        image(note1, this.x, this.y, 40, 50);
-    }
-    change() {
-        if (this.x>160 && this.x <200){
-        image(note2, this.x, this.y, noteW, noteH);
+    Note();
+    if (x1 > 160 && x1 < 200) {
+        image(noteFire, x1, y1, noteW, noteH);
         noteW += 2;
         noteH += 2.5;
     }
 
 }
-}
+
+function Note() {
+    image(note1, x1, y1, 40, 50);
+    x1 = x1 - 5;
+    if (x1<0){
+        x1=1500;
+    }
+    image(note0, x2, y2, 40, 50);
+    x2 = x2 - 5;
+    if (x2<0){
+        x2=1700;
+    }
+    }
+
+
