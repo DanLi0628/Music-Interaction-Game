d725fcf5d4e4ec2e78aa35ba2d6a1e29399271bc
Dan Li <dl4224@nyu.edu>
2020-05-07 12:14:10 +0800

    add noteCircle

--- sketch.js
+++ sketch.js
@@ -1,19 +1,14 @@
-let wxp;
-let backgroundImage;
-let note;
-noteW = 40;
-noteH = 50;
 let x1 = 1500;
 let y1 = 430;
-let x2 = x1 + 100;
+let x2 = x1 + 200;
 let y2 = 430;
-let x3 = x2 + 200;
+let x3 = x2 + 250;
 let y3 = 430;
-let x4 = x3 + 50;
+let x4 = x3 + 100;
 let y4 = 430;
-let x5 = x4 + 100;
+let x5 = x4 + 150;
 let y5 = 430;
-let x6 = x5 + 150;
+let x6 = x5 + 200;
 let y6 = 430;
 
 
@@ -27,6 +22,7 @@ function preload() {
     note4 = loadImage('background/note4.png');
     note5 = loadImage('background/note5.png');
     noteFire = loadImage('background/noteFire.png');
+    noteCircle = loadImage("background/noteCircle.png");
 }
 
 function setup() {
@@ -35,52 +31,50 @@ function setup() {
 function draw() {
     image(backgroundImage, 0, 0, windowWidth, windowHeight);
     image(wxp, 0, 430, windowWidth, 50);
+    image(noteCircle,160,425,70,70);
     Note();
-
+    noteW = 40;
+    noteH = 50;
     if (x1 > 160 && x1 < 200) {
-        noteW = 40;
-        noteH = 50;
         image(noteFire, x1, y1, noteW, noteH);
-        noteW += 2;
-        noteH += 2.5;
     }
     if (x2 > 160 && x2 < 200) {
-        noteW = 40;
-        noteH = 50;
         image(noteFire, x2, y2, noteW, noteH);
-        noteW += 2;
-        noteH += 2.5;
     }
     if (x3 > 160 && x3 < 200) {
-        noteW = 40;
-        noteH = 50;
         image(noteFire, x3, y3, noteW, noteH);
-        noteW += 2;
-        noteH += 2.5;
     }
     if (x4 > 160 && x4 < 200) {
-        noteW = 40;
-        noteH = 50;
         image(noteFire, x4, y4, noteW, noteH);
-        noteW += 2;
-        noteH += 2.5;
     }
     if (x5 > 160 && x5 < 200) {
-        noteW = 40;
-        noteH = 50;
         image(noteFire, x5, y5, noteW, noteH);
-        noteW += 2;
-        noteH += 2.5;
     }
     if (x6 > 160 && x6 < 200) {
-        noteW = 40;
-        noteH = 50;
         image(noteFire, x6, y6, noteW, noteH);
-        noteW += 2;
-        noteH += 2.5;
     }
-
 }
+
+// function mousePressed(){
+//     if (x1 > 160 && x1 < 200) {
+//         image(noteFire, x1, y1, noteW, noteH);
+//     }
+//     if (x2 > 160 && x2 < 200) {
+//         image(noteFire, x2, y2, noteW, noteH);
+//     }
+//     if (x3 > 160 && x3 < 200) {
+//         image(noteFire, x3, y3, noteW, noteH);
+//     }
+//     if (x4 > 160 && x4 < 200) {
+//         image(noteFire, x4, y4, noteW, noteH);
+//     }
+//     if (x5 > 160 && x5 < 200) {
+//         image(noteFire, x5, y5, noteW, noteH);
+//     }
+//     if (x6 > 160 && x6 < 200) {
+//         image(noteFire, x6, y6, noteW, noteH);
+//     }
+// }
 
 function Note() {
     image(note1, x1, y1, 40, 50);
